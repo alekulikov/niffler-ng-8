f33dc6f850a7f15348ce51bff283708aa72c09a5
alekulikov <99183267+alekulikov@users.noreply.github.com>
2025-04-02 11:42:13 +0300

    lesson_3.1 (#1)
    * lesson_3.1 - complete hw
    
    * lesson_3.1 - refactoring
    
    ---------
    
    Co-authored-by: alekulikov <aleksandr.a.kulikov@mvideo.ru>

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -0,0 +1,30 @@
+package guru.qa.niffler.test.web;
+
+import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.config.Config;
+import guru.qa.niffler.jupiter.BrowserExtension;
+import guru.qa.niffler.page.LoginPage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(BrowserExtension.class)
+public class LoginTest {
+
+  private static final Config CFG = Config.getInstance();
+
+  @Test
+  void mainPageShouldBeDisplayedAfterSuccessLogin() {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+        .doLogin("duck", "12345")
+        .checkMainPageBeenLoad();
+  }
+
+  @Test
+  void userShouldStayOnLoginPageAfterLoginWithBadCredentials() {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+        .doLogin("duck", "123456");
+    assertEquals("Login to Niffler", Selenide.title());
+  }
+}
