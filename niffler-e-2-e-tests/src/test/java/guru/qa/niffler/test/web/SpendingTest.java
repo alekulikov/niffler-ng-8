ea8a543e9bbd6a1dedf7971dac9cc4939cdb2e66
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2025-04-04 19:08:00 +0300

    lesson_3.3 - complete hw

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -3,6 +3,7 @@ package guru.qa.niffler.test.web;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Spend;
+import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
 import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
@@ -15,12 +16,14 @@ public class SpendingTest {
 
   private static final Config CFG = Config.getInstance();
 
-  @Spend(
+  @User(
       username = "duck",
-      category = "Обучение",
-      description = "Обучение Niffler 2.0",
-      amount = 89000.00,
-      currency = CurrencyValues.RUB
+      spendings = @Spend(
+          category = "Обучение",
+          description = "Обучение Niffler 2.0",
+          amount = 89000.00,
+          currency = CurrencyValues.RUB
+      )
   )
   @Test
   void spendingDescriptionShouldBeUpdatedByTableAction(SpendJson spend) {
