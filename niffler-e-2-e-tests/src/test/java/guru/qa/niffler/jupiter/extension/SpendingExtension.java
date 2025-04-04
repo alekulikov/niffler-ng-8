4750450283bd3ddfff6a8998dc843acb43c8f677
alekulikov <99183267+alekulikov@users.noreply.github.com>
2025-04-04 23:07:08 +0300

    lesson_3.3 - complete hw (#3)
    Co-authored-by: alekulikov <aleksandr.a.kulikov@mvideo.ru>

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendingExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendingExtension.java
@@ -1,14 +1,10 @@
 package guru.qa.niffler.jupiter.extension;
 
 import guru.qa.niffler.api.SpendApiClient;
-import guru.qa.niffler.jupiter.annotation.Spend;
+import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
-import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.support.AnnotationSupport;
 
 import java.util.Date;
@@ -20,25 +16,26 @@ public class SpendingExtension implements BeforeEachCallback, ParameterResolver 
 
   @Override
   public void beforeEach(ExtensionContext context) {
-    AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), Spend.class)
+    AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), User.class)
         .ifPresent(anno -> {
-          SpendJson spendJson = new SpendJson(
-              null,
-              new Date(),
-              new CategoryJson(
-                  null,
-                  anno.category(),
-                  anno.username(),
-                  false
-              ),
-              anno.currency(),
-              anno.amount(),
-              anno.description(),
-              anno.username()
-          );
-
-          SpendJson created = spendApiClient.addSpend(spendJson);
-          context.getStore(NAMESPACE).put(context.getUniqueId(), created);
+          if (anno.spendings().length > 0) {
+            SpendJson spendJson = new SpendJson(
+                null,
+                new Date(),
+                new CategoryJson(
+                    null,
+                    anno.spendings()[0].category(),
+                    anno.username(),
+                    false
+                ),
+                anno.spendings()[0].currency(),
+                anno.spendings()[0].amount(),
+                anno.spendings()[0].description(),
+                anno.username()
+            );
+            SpendJson created = spendApiClient.addSpend(spendJson);
+            context.getStore(NAMESPACE).put(context.getUniqueId(), created);
+          }
         });
   }
 
