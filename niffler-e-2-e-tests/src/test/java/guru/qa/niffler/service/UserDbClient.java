1b18236c02af1f9a375f770c0659f4d6b6795d46
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2025-04-23 22:30:06 +0300

    lesson_5.2 - complete hw

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UserDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/UserDbClient.java
@@ -8,10 +8,14 @@ import guru.qa.niffler.data.dao.impl.*;
 import guru.qa.niffler.data.entity.auth.Authority;
 import guru.qa.niffler.data.entity.auth.AuthorityEntity;
 import guru.qa.niffler.data.entity.auth.UserEntity;
+import guru.qa.niffler.data.tpl.DataSources;
 import guru.qa.niffler.data.tpl.XaTransactionTemplate;
 import guru.qa.niffler.model.UserDataJson;
+import org.springframework.data.transaction.ChainedTransactionManager;
+import org.springframework.jdbc.support.JdbcTransactionManager;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.util.Arrays;
 
@@ -30,6 +34,13 @@ public class UserDbClient {
   private final XaTransactionTemplate xaTransactionTemplate = new XaTransactionTemplate(
       CFG.authJdbcUrl(),
       CFG.userdataJdbcUrl()
+  );
+
+  private final TransactionTemplate chainedTrxSpring = new TransactionTemplate(
+      new ChainedTransactionManager(
+          new JdbcTransactionManager(DataSources.dataSource(CFG.authJdbcUrl())),
+          new JdbcTransactionManager(DataSources.dataSource(CFG.userdataJdbcUrl()))
+      )
   );
 
   public UserDataJson createUserSpringJdbc(UserDataJson user) {
@@ -88,5 +99,33 @@ public class UserDbClient {
               udUserDao.create(guru.qa.niffler.data.entity.userdata.UserEntity.fromJson(user)));
         }
     );
+  }
+
+  public UserDataJson createUserSpringJdbcChainedTrx(UserDataJson user) {
+    return chainedTrxSpring.execute(status -> {
+      UserEntity authUser = new UserEntity();
+      authUser.setUsername(user.username());
+      authUser.setPassword(pe.encode("12345"));
+      authUser.setEnabled(true);
+      authUser.setAccountNonExpired(true);
+      authUser.setAccountNonLocked(true);
+      authUser.setCredentialsNonExpired(true);
+
+      UserEntity createdAuthUser = authUserDaoSpring.create(authUser);
+
+      AuthorityEntity[] authorityEntities = Arrays.stream(Authority.values()).map(
+          e -> {
+            AuthorityEntity ae = new AuthorityEntity();
+            ae.setUser(createdAuthUser);
+            ae.setAuthority(e);
+            return ae;
+          }
+      ).toArray(AuthorityEntity[]::new);
+
+      authAuthorityDaoSpring.create(authorityEntities);
+
+      return UserDataJson.fromEntity(
+          udUserDaoSpring.create(guru.qa.niffler.data.entity.userdata.UserEntity.fromJson(user)));
+    });
   }
 }
