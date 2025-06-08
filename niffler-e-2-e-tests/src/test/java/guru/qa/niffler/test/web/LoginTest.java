0c29d5d16e2f1146915b157d8333fa1fcd3b4322
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2025-06-08 14:26:52 +0300

    lesson_10.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -1,24 +1,35 @@
 package guru.qa.niffler.test.web;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.User;
-import guru.qa.niffler.jupiter.annotation.meta.WebTest;
+import guru.qa.niffler.jupiter.extension.NonStaticBrowserExtension;
 import guru.qa.niffler.model.UserDataJson;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+import java.util.List;
+
+import static guru.qa.niffler.utils.SelenideUtils.chromeConfig;
+import static guru.qa.niffler.utils.SelenideUtils.firefoxConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@WebTest
 public class LoginTest {
 
   private static final Config CFG = Config.getInstance();
 
+  @RegisterExtension
+  private final NonStaticBrowserExtension browserExtension = new NonStaticBrowserExtension();
+  private final SelenideDriver chromeDriver = new SelenideDriver(chromeConfig);
+
   @User
   @Test
   void mainPageShouldBeDisplayedAfterSuccessLogin(UserDataJson user) {
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
+    browserExtension.drivers().add(chromeDriver);
+
+    chromeDriver.open(CFG.frontUrl());
+    new LoginPage(chromeDriver)
         .doLogin(user.username(), user.testData().password())
         .checkMainPageBeenLoad();
   }
@@ -26,8 +37,13 @@ public class LoginTest {
   @User
   @Test
   void userShouldStayOnLoginPageAfterLoginWithBadCredentials(UserDataJson user) {
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .doLogin(user.username(), "123456");
-    assertEquals("Login to Niffler", Selenide.title());
+    SelenideDriver firefoxDriver = new SelenideDriver(firefoxConfig);
+    browserExtension.drivers().addAll(List.of(chromeDriver, firefoxDriver));
+
+    chromeDriver.open(CFG.frontUrl());
+    new LoginPage(chromeDriver).doLogin(user.username(), "123456");
+    assertEquals("Login to Niffler", chromeDriver.title());
+    firefoxDriver.open(CFG.frontUrl());
+    new LoginPage(firefoxDriver).goRegisterPage();
   }
 }
